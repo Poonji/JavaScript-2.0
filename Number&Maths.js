09f1f59327b268aca2f1dab33ea6ddd2888b207e
Poonji <poonjij21@gmail.com>
2023-11-09 13:58:23 +0530

    maths in js

--- Number&Maths.js
+++ Number&Maths.js
@@ -5,3 +5,11 @@ const balance=new Number(100)
 console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
+
+//**************************MATHS*****************
+
+console.log(Math);
+console.log(Math.min(3,4345,4,5,4,5,3,42,423,3,54,));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);//to genrate number before decimal
