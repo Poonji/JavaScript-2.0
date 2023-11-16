9049984bf614cc3b85b5f0771fef062e924a0a52
Poonji <poonjij21@gmail.com>
2023-11-17 01:33:54 +0530

    objects in js

---
+++ array.js
@@ -0,0 +1,3 @@
+const another_array=[1,2,3,[4,,5,6],7,[6,7,8,5,[4,7]]]
+const real_array=another_array.flat(Infinity)
+console.log(real_array); 
---
+++ objects.js
@@ -0,0 +1,11 @@
+const JsUser={
+    name:"Poonji",
+    age:18,
+    location:"Prayagraj",
+    email:"poonjij21@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(JsUser.name)
+console.log()
