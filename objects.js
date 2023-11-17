0786c9e627e63436b0f49e481507b6c739c9e994
Poonji <poonjij21@gmail.com>
2023-11-17 18:28:11 +0530

    objects2

--- objects.js
+++ objects.js
@@ -8,4 +8,21 @@ const JsUser={
 }
 
 console.log(JsUser.name)
-console.log()
+console.log(JsUser["email"])
+
+// --------------------Objects nesting.............................
+
+const regularUser=
+{
+    email:"someone@gmail.com",
+    name:"someone",
+    fullname:{
+        userfullname:{
+            firstname:"ambar",
+            lastname:"mishra"
+        }
+
+    }
+
+}
+console.log(regularUser.fullname.userfullname.firstname);
