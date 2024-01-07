5746f6b6d09c47e8f530e57b211c0b294c25a4a3
Poonji <poonjij21@gmail.com>
2024-01-07 23:19:41 +0530

    filter

--- this&arrowfunction.js
+++ this&arrowfunction.js
@@ -7,9 +7,32 @@ const user={
         console.log(`${this.username}, welcome to website`);
         // this keyword points towards
         // current variable
+       // console.log(`${this.price},is the price`);
     }
 }
 
 user.welcomeMessage()
 user.username="sam"
 user.welcomeMessage()
+//user.price=38237
+//user.welcomeMessage()
+//--------------arrow function-------
+
+const chai=()=>{
+    let username="poonji"
+    console.log(this);
+}
+
+chai()
+
+//-------pure use of arrow function-------
+
+const addtwo=(num1,num2)=>{
+   return num1+num2
+}
+
+console.log(addtwo(5,3))
+// if you apply { } then you have to write a return keyword
+const addtwo0=(num1,num2)=>num1+num2
+
+console.log(addtwo0(3,4))
