a25e394d15d304fda59c9096385cb062488fc9fd
Poonji <poonjij21@gmail.com>
2024-02-10 14:37:00 +0530

    events in js

--- filter,map,reduce.js
+++ filter,map,reduce.js
@@ -3,4 +3,12 @@ const myNums=[1,2,3,4,5,6,7,8,9,10]
 const newNums=myNums.filter((num)=>num>5)
 console.log(newNums);
 
+//reduce 
+// it reduces the array to a single value
+const arr=[1,2,3,4,5,6,6]
+let newarr=arr.reduce((n1,n2)=>{
+    return n1+n2
+})
+console.log(newarr)
+
 
